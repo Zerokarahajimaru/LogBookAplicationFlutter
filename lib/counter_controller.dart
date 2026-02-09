b73fbd4fa1a439d2ff15934c23d8cc8d20db6b18
ZerokaraHajimaru <yukannahinoishi@gmail.com>
2026-02-09 11:08:43 +0700

    added function to add history to list

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,6 +1,8 @@
 class CounterController {
   int _counter = 0;
   int _step = 1;
+  List<String> _history = [];
+  List<DateTime> _time_history = [];
 
   int get value => _counter;
   int get step => _step;
@@ -16,5 +18,10 @@ class CounterController {
   void reset() {
     _counter = 0;
     _step = 1;
+  }
+
+  void history(Object input){
+    _history.add(input.toString());
+    _time_history.add(DateTime.now());
   }
 }
